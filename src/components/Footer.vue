2a7e39c640073843ec5a7d3132f0ffc387ce1695
Nelson M <nelson.mccullough@peloton.com>
2021-05-08 16:04:53 -0600

    update target

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="mb-5">
         <v-layout class="wiggly" justify-center>
-            <v-btn icon href="https://www.instagram.com/inoahlad/">
+            <v-btn icon href="https://www.instagram.com/inoahlad/" target="__blank">
                 <v-icon>mdi-instagram</v-icon>
             </v-btn>
-            <v-btn icon href="https://www.linkedin.com/in/noah-dashney/">
+            <v-btn icon href="https://www.linkedin.com/in/noah-dashney/" target="__blank">
                 <v-icon>mdi-linkedin</v-icon>
             </v-btn>
             <!-- <v-btn icon href="#">
                 <v-icon>mdi-artstation</v-icon>
             </v-btn> -->
-            <v-btn icon href="https://www.youtube.com/channel/UCVQVOTTgEMNatT1UTQHvrsA">
+            <v-btn icon href="https://www.youtube.com/channel/UCVQVOTTgEMNatT1UTQHvrsA" target="__blank">
                 <v-icon>mdi-youtube</v-icon>
             </v-btn>
         </v-layout>
