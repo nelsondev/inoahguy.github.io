5d888a4b1d9bac59ec0dd9735d291df18664be7d
Nelson M <nelson.mccullough@peloton.com>
2021-05-08 22:27:47 -0600

    moved to nuxt

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,9 +7,6 @@
             <v-btn icon href="https://www.linkedin.com/in/noah-dashney/" target="__blank">
                 <v-icon>mdi-linkedin</v-icon>
             </v-btn>
-            <!-- <v-btn icon href="#">
-                <v-icon>mdi-artstation</v-icon>
-            </v-btn> -->
             <v-btn icon href="https://www.youtube.com/channel/UCVQVOTTgEMNatT1UTQHvrsA" target="__blank">
                 <v-icon>mdi-youtube</v-icon>
             </v-btn>
